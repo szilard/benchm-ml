8290d1f09d50c9609138c2d35c77da382a87cdb1
szilard <szilard@noemail>
2015-04-28 09:19:44 -0700

    linear

--- 1-linear/z-auc-lin-rf.R
+++ 1-linear/z-auc-lin-rf.R
@@ -4,7 +4,7 @@ library(readr)
 
 d <- read_csv("z-auc-lin-rf.csv")
 
-ggplot(d, aes(x = n, y = AUC, color = Tool)) +
+ggplot(d, aes(x = n, y = AUC, color = Model)) +
   geom_point() + geom_line() + scale_x_log10(breaks = c(0.01,0.1,1,10))
 
 
