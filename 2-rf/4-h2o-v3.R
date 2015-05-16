44c8e95c3eff25b4f268899b147399b85ac51de0
szilard <szilard@users.noreply.github.com>
2015-05-15 22:22:02 -0700

    h2o 3.0

---
+++ 2-rf/4-h2o-v3.R
@@ -0,0 +1,29 @@
+
+library(h2o)
+
+h2oServer <- h2o.init(max_mem_size="60g", nthreads=-1)
+
+dx_train <- h2o.importFile(h2oServer, path = "train-1m.csv")
+dx_test <- h2o.importFile(h2oServer, path = "test.csv")
+
+for (k in c("Month","DayofMonth","DayOfWeek")) {
+  dx_train[[k]] <- as.factor(dx_train[[k]])
+  dx_test[[k]] <- as.factor(dx_test[[k]])
+}
+
+dx_train
+summary(dx_train)
+
+Xnames <- names(dx_train)[which(names(dx_train)!="dep_delayed_15min")]
+
+
+system.time({
+  md <- h2o.randomForest(x = Xnames, y = "dep_delayed_15min", training_frame = dx_train, ntrees = 500)
+})
+
+
+h2o.auc(h2o.performance(md, dx_test))
+
+
+
+
