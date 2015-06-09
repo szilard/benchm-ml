93eec56de84bf90d4c7297774dcc46fecef157e5
szilard <szilard@noemail>
2015-06-09 10:29:10 -0700

    h2o3

---
+++ 3-boosting/4-h2o-v3.R
@@ -0,0 +1,30 @@
+
+library(h2o)
+
+h2oServer <- h2o.init(max_mem_size="60g", nthreads=-1)
+
+dx_train <- h2o.importFile(h2oServer, path = "train-1m.csv")
+dx_test <- h2o.importFile(h2oServer, path = "test.csv")
+
+for (k in c("Month","DayofMonth","DayOfWeek")) {
+  dx_train[[k]] <- as.factor(dx_train[[k]])
+  dx_test[[k]] <- as.factor(dx_test[[k]])
+}
+
+Xnames <- names(dx_train)[which(names(dx_train)!="dep_delayed_15min")]
+
+
+system.time({
+  md <- h2o.gbm(x = Xnames, y = "dep_delayed_15min", training_frame = dx_train, distribution = "bernoulli", 
+          ntrees = 1000, 
+          max_depth = 16, learn_rate = 0.01, min_rows = 1,
+          nbins = 100)
+})
+
+
+system.time({
+  print(h2o.auc(h2o.performance(md, dx_test)))
+})
+
+
+
