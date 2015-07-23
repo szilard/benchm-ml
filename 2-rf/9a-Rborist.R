65fcd5db1ff0d2bcdbd113a1e1f8a1f4b70110f4
szilard <szilard@users.noreply.github.com>
2015-07-23 12:58:44 -0700

    cleanup

--- 2-rf/9a-Rborist.R
+++ 2-rf/9a-Rborist.R
@@ -5,42 +5,38 @@ library(ROCR)
 dx_train <- as.data.frame(fread("train-1m.csv"))
 dx_test <- as.data.frame(fread("test.csv"))
 
-dt_train <- dx_train
-dt_test <- dx_test
-
 # Rborist 0-1.1 only accepts factor and numeric predictors or response:
 #
 facCols <- c("UniqueCarrier", "Origin","Dest", "Month", "DayofMonth", "DayOfWeek")
-
-responseCol <- "dep_delayed_15min"
 numCols <- c("DepTime","Distance")
+responseCol <- "dep_delayed_15min"
 
 for (k in facCols) {
-  dt_train[[k]] <- as.factor(dx_train[[k]])
-  dt_test[[k]] <- as.factor(dx_test[[k]])
+  dx_train[[k]] <- as.factor(dx_train[[k]])
+  dx_test[[k]] <- as.factor(dx_test[[k]])
 }
-
-dt_train[[responseCol]] <- as.factor(dx_train[[responseCol]])
-dt_test[[responseCol]] <- as.factor(dx_test[[responseCol]])
 
 for (k in numCols) {
-  dt_train[[k]] <- as.numeric(dx_train[[k]])
-  dt_test[[k]] <- as.numeric(dx_test[[k]])
+  dx_train[[k]] <- as.numeric(dx_train[[k]])
+  dx_test[[k]] <- as.numeric(dx_test[[k]])
 }
 
-Xnames <- names(dt_train)[which(names(dt_train) != responseCol)]
+dx_train[[responseCol]] <- as.factor(dx_train[[responseCol]])
+dx_test[[responseCol]] <- as.factor(dx_test[[responseCol]])
+
+Xnames <- names(dx_train)[which(names(dx_train) != responseCol)]
 
 
 
 system.time({
-  md <- Rborist(dt_train[, Xnames], dt_train[, responseCol], nTree = 100)
+  md <- Rborist(dx_train[, Xnames], dx_train[, responseCol], nTree = 100)
 })
 
 system.time({
-  phat <- predict(md, newdata=dt_test[, Xnames], ctgCensus="prob")$ctgCensus[,"Y"]
+  phat <- predict(md, newdata=dx_test[, Xnames], ctgCensus="prob")$ctgCensus[,"Y"]
 })
 
-rocr_pred <- prediction(phat, dt_test$dep_delayed_15min == "Y")
+rocr_pred <- prediction(phat, dx_test$dep_delayed_15min == "Y")
 performance(rocr_pred, "auc")
 
 
