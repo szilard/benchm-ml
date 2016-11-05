6de386f9c4af3e86587226e570090bbed4647b61
szilard <szilard@users.noreply.github.com>
2016-11-04 23:28:15 -0700

    DL

---
+++ 4-DL/1a-h2o-1model.R
@@ -0,0 +1,37 @@
+
+
+library(h2o)
+h2o.init(max_mem_size = "50g", nthreads = -1)
+
+
+dx_train <- h2o.importFile("train-1m.csv")
+dx_test <- h2o.importFile("test.csv")
+
+
+## to have same normalization as for the other DL libs that don't auto normalize 
+dx_train$DepTime <- dx_train$DepTime/2500
+dx_test$DepTime <- dx_test$DepTime/2500
+
+dx_train$Distance <- log10(dx_train$Distance)/4
+dx_test$Distance <- log10(dx_test$Distance)/4
+
+
+Xnames <- names(dx_train)[which(names(dx_train)!="dep_delayed_15min")]
+
+
+
+system.time({
+  md <- h2o.deeplearning(x = Xnames, y = "dep_delayed_15min", training_frame = dx_train,
+            activation = "Rectifier", hidden = c(200,200), 
+            adaptive_rate = FALSE, rate = 0.01, rate_annealing = 0,
+            momentum_start = 0.9, momentum_stable = 0.9, nesterov_accelerated_gradient = FALSE,
+            epochs = 1) 
+})
+h2o.performance(md, dx_test)@metrics$AUC
+
+#   user  system elapsed 
+#  0.750   0.008  51.358 
+#> h2o.performance(md, dx_test)@metrics$AUC
+#[1] 0.7226275
+
+
--- 4-DL/2-mxnet.R
+++ 4-DL/2-mxnet.R
@@ -4,42 +4,34 @@ library(mxnet)
 library(Matrix)
 
 d_train <- read_csv("train-1m.csv")
-d_valid <- read_csv("valid.csv")
 d_test <- read_csv("test.csv")
 
 
 ## normalization
 d_train$DepTime <- d_train$DepTime/2500
-d_valid$DepTime <- d_valid$DepTime/2500
 d_test$DepTime <- d_test$DepTime/2500
 
 d_train$Distance <- log10(d_train$Distance)/4
-d_valid$Distance <- log10(d_valid$Distance)/4
 d_test$Distance <- log10(d_test$Distance)/4
 
 
 system.time({
-  ##X_train_test_valid <- Matrix::sparse.model.matrix(dep_delayed_15min ~ ., data = rbind(d_train, d_test, d_valid))
-  X_train_test_valid <- model.matrix(dep_delayed_15min ~ ., data = rbind(d_train, d_test, d_valid))
-  X_train <- X_train_test_valid[1:nrow(d_train),]
-  X_test <- X_train_test_valid[(nrow(d_train)+1):(nrow(d_train)+nrow(d_test)),]
-  X_valid <- X_train_test_valid[(nrow(d_train)+nrow(d_test)+1):(nrow(d_train)+nrow(d_test)+nrow(d_valid)),]
+  X_train_test <- model.matrix(dep_delayed_15min ~ ., data = rbind(d_train, d_test))
+  X_train <- X_train_test[1:nrow(d_train),]
+  X_test <- X_train_test[(nrow(d_train)+1):(nrow(d_train)+nrow(d_test)),]
 })
-##dim(X_train_test_valid)
-##object.size(X_train_test_valid)/1e6
 
 
-mx.set.seed(0)
+mx.set.seed(123)
+
 system.time({
 md <- mx.mlp(X_train, as.numeric(d_train$dep_delayed_15min=="Y"), 
           array.layout = "rowmajor", out_node = 2, 
           device = mx.gpu(),
-          hidden = c(128,128), activation = "relu", ##dropout = 0.1,        
-          num.round = 2, array.batch.size = 100,
-          learning.rate = 0.07, momentum = 0.9, initializer = mx.init.uniform(0.07),
-          eval.metric = mx.metric.accuracy, 
-          eval.data = list(data = X_valid, label = as.numeric(d_valid$dep_delayed_15min=="Y"))
-          )
+          hidden = c(200,200), activation = "relu", 
+          num.round = 1, array.batch.size = 128,
+          learning.rate = 0.01, momentum = 0.9, initializer = mx.init.uniform(0.1),
+          eval.metric = mx.metric.accuracy)
 })
 
 
